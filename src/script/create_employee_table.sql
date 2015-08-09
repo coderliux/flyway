15a52b63bb0f27f8adb72d9b4dcd9e897e78fb7f
coderliux <ecliujie@gmail.com>
2015-08-09 21:41:59 +0800

    init by coderliu, initializing script and db setup

---
+++ src/script/create_employee_table.sql
@@ -0,0 +1,9 @@
+create table tb_employee(
+    id int primary key,
+    name varchar(255),
+    email varchar(255)
+);
+
+insert into tb_employee values(1, 'jie liu','jjliu@thoughtworks.com');
+insert into tb_employee values(2, 'tester','test@thoughtworks.com');
+
